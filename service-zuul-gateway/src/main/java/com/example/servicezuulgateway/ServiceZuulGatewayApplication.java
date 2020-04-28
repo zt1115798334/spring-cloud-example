1564e34cd13df8b570b05a68c95200922bb415ca
zhangtong <1234567890>
2020-04-28 16:26:52 +0800

    提交

---
+++ service-zuul-gateway/src/main/java/com/example/servicezuulgateway/ServiceZuulGatewayApplication.java
@@ -0,0 +1,17 @@
+package com.example.servicezuulgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@EnableEurekaClient
+@EnableZuulProxy
+@SpringBootApplication
+public class ServiceZuulGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceZuulGatewayApplication.class, args);
+    }
+
+}
---
+++ service-zuul-gateway/src/test/java/com/example/servicezuulgateway/ServiceZuulGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.servicezuulgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceZuulGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
